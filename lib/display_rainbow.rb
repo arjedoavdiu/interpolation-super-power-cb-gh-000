750fbf9ef611d9615457554a111a2f84ea98f308
Arjedo Avdiu <aavdiu2@uet.edu.al>
2018-03-30 14:29:48 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,10 @@
 # Write your #display_rainbow method here
+def display_rainbow(red, orange, yellow, green, blue, indigo, violet)
+  red = R
+  orange = O
+  yellow = Y
+  green = G
+  blue = B
+  indigo = I
+  violet = V
+end
